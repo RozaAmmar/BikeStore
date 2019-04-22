9c93cccf863998fe8c1da955050d9f065435bb46
RozaAmmar <49791997+RozaAmmar@users.noreply.github.com>
2019-04-22 11:00:57 +0300

    Add files via upload

---
+++ onlinebikestore/App_Code/Utility.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Utility
+/// </summary>
+public class Utility
+{
+
+    public void Createcookie(string cookiename, string[] keys, string[] values, bool expire,HttpResponse res)
+    {
+        HttpCookie c = new HttpCookie(cookiename);
+        if (keys != null)
+        {
+            for (int x = 0; x < keys.Length; x++)
+                c.Values.Add(keys[x], values[x]);
+
+            if (!expire)
+
+                c.Expires = DateTime.Now.AddYears(5);
+        }
+        else
+        {
+            c.Expires = DateTime.Now.AddYears(-5);
+        }
+            res.Cookies.Add(c);
+ 
+        
+
+    }
+
+    public string ReadFromCookie(string cookiename,string key,HttpRequest req)
+    {
+        try
+        {
+
+            return req.Cookies[cookiename][key].ToString();
+        }
+        catch
+        {
+            return null;
+        }
+        
+    }
+
+
+    public void Removecookie(string cookiename,  HttpResponse   res)
+    {
+        Createcookie(cookiename, null, null,false,res);
+    }
+    public Utility()
+	{
+		
+	}
+}
---
+++ onlinebikestore/App_Code/adm_class.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AnalysisServices.AdomdClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for adm_class
+/// </summary>
+public class adm_class
+{
+	public adm_class()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public List<string> predict(List<string> input)
+    {
+        List<string> output = new List<string>();
+        AdomdConnection CON = new AdomdConnection(
+            "Data Source=.;Catalog=AnalysisDB");
+
+        CON.Open();
+
+        AdomdCommand COM = CON.CreateCommand();
+
+        string s = @"SELECT Flattened  PREDICT([Orders Details],3)
+           FROM [OrdersMM] 
+          NATURAL PREDICTION JOIN
+          (SELECT ( ";
+
+        foreach (string x in input)
+        {
+            if (input.IndexOf(x) > 0)
+                s += " Union ";
+            s += "Select '" + x + "' as [Product Name]";
+        }
+
+        s += " )  AS [Orders Details]) As T";
+
+
+        COM.CommandText = s;
+
+
+
+        AdomdDataReader DR = COM.ExecuteReader();
+
+        while (DR.Read())
+        {
+            if (DR[0] != null)
+                output.Add(DR[0].ToString());
+
+        }
+
+        DR.Close();
+        CON.Close();
+        return output;
+    }
+}
